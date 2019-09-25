ea54dceb2bf55a73a3bbc1c0a29c8274371ea431
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2019-09-25 14:20:31 -0700

    Formatting.

--- src/system/kernel/device_manager/IORequest.cpp
+++ src/system/kernel/device_manager/IORequest.cpp
@@ -881,7 +881,7 @@ IORequest::FinishedCallback(void** _cookie) const
 status_t
 IORequest::Wait(uint32 flags, bigtime_t timeout)
 {
-  TRACE("%p->IORequest::Wait(%d, %ld)\n", this, flags, timeout);
+	TRACE("%p->IORequest::Wait(%d, %ld)\n", this, flags, timeout);
 
 	MutexLocker locker(fLock);
 
